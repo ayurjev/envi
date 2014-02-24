41de3a0cc3c97a874340891db646305711cea10b
ayurjev <andrey.yurjev@gmail.com>
2014-02-24 16:09:06 +0400

    Добавлен импорт класса Response в общее пространство имен envi

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-from envi.classes import Application, Controller, StaticPipe, AjaxPipe, PjaxPipe, JsonRpcPipe, Request
+from envi.classes import Application, Controller, StaticPipe, AjaxPipe, PjaxPipe, JsonRpcPipe, Request, Response
