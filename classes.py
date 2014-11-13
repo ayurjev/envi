554e19e5ef008069f5f843c12067b5015ccb1bbd
ayurjev <andrey.yurjev@gmail>
2014-11-13 16:15:44 +0400

    Запись лога в файл - блокирующая операция убивающая производительность при 100 воркерах напроч... Надо будет подумать о решении, но пока выключаю

--- classes.py
+++ classes.py
@@ -75,19 +75,19 @@ class Application(bottle.Bottle):
         #     response_template_params["response"] = response_template_params["response"][:128]
 
         # noinspection PyBroadException
-        try:
-            with open("/tmp/envi-{host}.log".format(host=request.host), "a") as log_file:
-                short_request = {key: value for key, value in request.items()}
-                log_file.write(
-                    event_template.format(
-                        datetime=datetime.today(),
-                        request=request,
-                        request_str=str(short_request),
-                        response="{type}({size}) {response}".format(**response_template_params)
-                    )
-                )
-        except IOError:
-            return
+        # try:
+        #     with open("/tmp/envi-{host}.log".format(host=request.host), "a") as log_file:
+        #         short_request = {key: value for key, value in request.items()}
+        #         log_file.write(
+        #             event_template.format(
+        #                 datetime=datetime.today(),
+        #                 request=request,
+        #                 request_str=str(short_request),
+        #                 response="{type}({size}) {response}".format(**response_template_params)
+        #             )
+        #         )
+        # except IOError:
+        #     return
 
     # noinspection PyMethodOverriding
     def route(self, path, controller, action=None):
