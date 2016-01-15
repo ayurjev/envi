d2d13663d22da94bcc4f457f0c8edb6f5d4001d2
Niev <evselyev@yandex.ru>
2016-01-15 20:06:32 +0300

    SuitApplication

--- envi/classes.py
+++ envi/classes.py
@@ -174,6 +174,34 @@ class Application(bottle.Bottle):
             return []
 
 
+class SuitApplication(Application):
+    def ajax_output_converter(self, result) -> dict:
+        """ Функция для конвертации ответов при ajax запросах
+        Настраиваем формат положительных и отрицательных результатов ajax-запросов
+        :param result: Экземпляр исключения (Exception) или Словарь с данными (dict)
+        """
+        if isinstance(result, Exception):
+            exc = result
+            match = re.search("'(.+)'", str(exc.__class__))
+            err_type = match.group(1) if match else exc.__class__.__name__
+            result = {"error": {"type": err_type, "message": str(exc), "data": {}}}
+            return result
+        else:
+            return {"result": result}
+
+    def static_output_converter(self, result: ControllerMethodResponseWithTemplate) -> str:
+        """ Функция для конвертации ответов при статических загрузках страницы
+        Подключаем Suit в качестве кастомной шаблонизации
+        :param result: Ответ в формате ControllerMethodResponseWithTemplate
+        """
+        from suit.Suit import Suit, TemplateNotFound
+        # noinspection PyBroadException
+        try:
+            return Suit(result.template).execute(result.data)
+        except TemplateNotFound:
+            return str(result)
+
+
 class Controller(metaclass=ABCMeta):
     error_template = "error_template"
     default_action = "not_implemented"
