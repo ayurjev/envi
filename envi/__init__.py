a47dfc1d61424f8831993eeb5000482069409811
Niev <evselyev@yandex.ru>
2016-01-15 20:13:35 +0300

    __init__.py

--- envi/__init__.py
+++ envi/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '0.1'
 
-from envi.classes import Application, Controller, ProxyController, WebSocketController, \
+from envi.classes import Application, SuitApplication, Controller, ProxyController, WebSocketController, \
     RequestPipe, JsonRpcRequestPipe,\
     Request, Response,\
     template, ControllerMethodResponseWithTemplate
