302f26135be13039c8e7d45ff5bd7e06e151491e
Niev <evselyev@yandex.ru>
2016-11-01 16:06:03 +0300

    compile distribution 0.2

--- envi/__init__.py
+++ envi/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1'
+__version__ = '0.2'
 
 from envi.classes import Application, SuitApplication, Controller, ProxyController, WebSocketController, \
     RequestPipe, JsonRpcRequestPipe,\
